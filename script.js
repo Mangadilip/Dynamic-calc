3202aeebc9fcc6fc322a776e411994dfe00af80c
mangadilip <mangadileep42@gmail.com>
2025-01-28 07:30:30 +0530

    expression is add

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ let display=document.getElementById("display")
 function calc(event){
     display.value+=event.target.value
 }
-function my_evaluate(){
-    display.value=eval(display.value)
+function my_evaluate() {
+    try {
+        
+        let expression = display.value.replace(/%/g, "/100");
+        display.value = eval(expression); 
+    } catch (error) {
+        display.value = "Error"; 
+    }
 }
 function del(){
     display.value=display.value.slice(0,-1)
